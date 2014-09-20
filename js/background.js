4e94a677a416dd2b76e5cc70755e7ac572da08f5
Tyler Sanderson <tylerbtbam@gmail.com>
2014-09-20 10:01:43 -0400

    Added wave and rest gestures along with unlock and lock support after 3s

--- js/background.js
+++ js/background.js
@@ -1,5 +1,15 @@
+//Address where the Myo Web Socket API is listening for requests
 var host = "ws://127.0.0.1:7204/myo/1";
+
+//Regex to differentiate regular windows from the background script window
+var bgPattern = /Developer Tools - chrome-extension/;
+
 var myoID = -1;
+
+var unlocked = false;
+var resting = true;
+var lastGestureTimeStamp = 0;
+
 console.log("Host:", host);
 
 var s = new WebSocket(host);
@@ -17,31 +27,36 @@ s.onmessage = function (e) {
 	var json = JSON.parse(e.data);
 	var data = json[1];
 	
+	console.log(parseInt(data.timestamp - lastGestureTimeStamp) / 1000000);
+	if (unlocked && resting && (parseInt(data.timestamp) - lastGestureTimeStamp) / 1000000 > 3){
+		console.log("Locking!");
+		unlocked = false;
+		requestVibrate();
+	}
+	
 	if (data.type !== "orientation"){
-		console.log(json[1]);
-		if (data.type === "pose" && data.pose === "thumb_to_pinky" && myoID != -1){
-			requestVibrate();
+		console.log(e.data);
+	}
+	
+	if (data.type === "pose" && myoID != -1){
+	
+		if (data.pose === "rest"){
+			onRest(data);
 		}
-		if (data.type === "pose" && data.pose === "wave_in" && myoID != -1){
-			//TODO;
+		else if (data.pose === "thumb_to_pinky"){
+			onThumbToPinky(data);
 		}
-		if (data.type === "pose" && data.pose === "wave_out" && myoID != -1){
-			//TODO;
-		}
-		if (data.type === "pose" && data.pose === "fist" && myoID != -1){
-			var pattern = /Developer Tools - chrome-extension/;
-			var idToClose = -1;
-			chrome.windows.getLastFocused({populate: true}, function(window){
-				var tabs = window.tabs;
-				for (var j = 0; j < tabs.length; ++j){
-					if (tabs[j].active && tabs[j].title.search(/Developer Tools/) === -1){
-						idToClose = tabs[j].id;
-					}
-					console.log(tabs[j]);
-					console.log(tabs[j].title.search(/Developer Tools/));
-				}
-				chrome.tabs.remove(idToClose);
-			});
+		
+		else if (unlocked){
+			lastGestureTimeStamp = parseInt(data.timestamp);
+			if (data.pose === "wave_in")
+				onWaveIn(data);
+			else if (data.pose === "wave_out")
+				onWaveOut(data);
+			else if (data.pose === "fist")
+				onFist(data);
+			else if (data.pose === "fingers_spread")
+				onFingersSpread(data);
 		}
 	}
 	
@@ -53,6 +68,70 @@ s.onmessage = function (e) {
 s.onerror = function (e) {
 	console.log("Socket error:", e);
 };
+
+function onThumbToPinky(data){
+	if (!unlocked){
+		lastGestureTimeStamp = parseInt(data.timestamp);
+		unlocked = true;
+	}
+	else{
+		unlocked = false;
+	}
+	requestVibrate();
+}
+
+function onWaveIn(data){
+	chrome.windows.getLastFocused({populate: true}, function(window){
+		var tabs = window.tabs;
+		var selectedIndex;
+		for (var i = 0; i < tabs.length; ++i){
+		    if (tabs[i].active && tabs[i].title.search(bgPattern === -1)){
+			    selectedIndex = i;
+			}
+		}
+		chrome.tabs.highlight({windowId: window.id, tabs: (selectedIndex === 0) ? tabs.length - 1 : selectedIndex - 1}, function(window){});
+	});
+	resting = false;
+}
+
+function onWaveOut(data){
+	chrome.windows.getLastFocused({populate: true}, function(window){
+		var tabs = window.tabs;
+		var selectedIndex;
+		for (var i = 0; i < tabs.length; ++i){
+		    if (tabs[i].active && tabs[i].title.search(bgPattern === -1)){
+			    selectedIndex = i;
+			}
+		}
+		chrome.tabs.highlight({windowId: window.id, tabs: (selectedIndex === tabs.length - 1) ? 0 : selectedIndex + 1}, function(window){});
+	});
+	resting = false;
+}
+
+function onFist(data){
+	var idToClose = -1;
+	chrome.windows.getLastFocused({populate: true}, function(window){
+		var tabs = window.tabs;
+		for (var j = 0; j < tabs.length; ++j){
+			if (tabs[j].active && tabs[j].title.search(bgPattern) === -1){
+				idToClose = tabs[j].id;
+			}
+		}
+		chrome.tabs.remove(idToClose);
+	});
+	resting = false;
+}
+
+function onFingersSpread(data){
+	chrome.windows.getLastFocused({populate: true}, function(window){
+		chrome.tabs.create({windowId: window.id, active: true});
+	});
+	resting = false;
+}
+
+function onRest(data){
+    resting = true;
+}
 
 function requestVibrate(){
 	var data = [
